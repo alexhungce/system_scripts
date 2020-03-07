97a066d72de7755cbe20408bb6fd5dbf2ae778d1
Alex Hung <alexhung@gmail.com>
2020-03-06 20:49:02 -0700

    setup_init: auto install spotify
    Signed-off-by: Alex Hung <alexhung@gmail.com>

--- setup_init.sh
+++ setup_init.sh
@@ -56,7 +56,7 @@ fi
 # install spotify
 curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt update && sudo apt install spotify-client
+sudo apt update && sudo apt install -y spotify-client
 
 # install applications for Gnome DE
 sudo apt install -y gnome-tweaks chrome-gnome-shell
