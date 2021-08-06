1975992fc66b3edde145090d0284d00477f3db74
Alex Hung <alexhung@gmail.com>
2021-08-06 00:35:27 -0600

    setup_init: remove spotify
    Signed-off-by: Alex Hung <alexhung@gmail.com>

--- setup_init.sh
+++ setup_init.sh
@@ -52,11 +52,6 @@ if [ $? != 0 ] ; then
 			    powertop ubuntu-restricted-addons steam \
 			    avahi-daemon
 
-	# install spotify
-	curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
-	echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-	sudo apt update && sudo apt install -y spotify-client
-
 fi
 
 # remove pre-installed applications
