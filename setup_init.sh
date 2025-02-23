7960eecb740b01dc9a5c653e9acf015a0b528011
Alex Hung <alexhung@gmail.com>
2025-02-22 20:01:31 -0700

    setup_init: Merge server_setup.sh and refactor
    Signed-off-by: Alex Hung <alexhung@gmail.com>

--- setup_init.sh
+++ setup_init.sh
@@ -1,85 +1,157 @@
 #!/bin/bash
 shopt -s -o nounset
 
+generic_install_packages () {
+	sudo apt install -y acpi \
+			    acpica-tools \
+			    avahi-daemon \
+			    btop \
+			    crudini \
+			    curl \
+			    fd-find \
+			    fzf \
+			    git \
+			    git-email \
+			    gitk \
+			    htop \
+			    iw \
+			    lm-sensors \
+			    libfuse-dev \
+			    nvme-cli \
+			    nvtop \
+			    openssh-server \
+			    openconnect \
+			    p7zip-full \
+			    pastebinit \
+			    plocate \
+			    python3-github \
+			    python3-gpg \
+			    python3-virtualenv \
+			    screen \
+			    shellcheck \
+			    tree \
+			    unp \
+			    vim
+}
+
+destkop_install_packages () {
+	sudo apt install -y drm-info \
+			   deluge \
+			   ibus-chewing \
+			   linux-tools-generic \
+			   linux-tools-`uname -r` \
+			   meld \
+			   mpv \
+			   numix-gtk-theme \
+			   numix-icon-theme-circle \
+			   powertop \
+			   preload \
+			   remmina \
+			   thunderbird \
+			   tilix \
+			   ubuntu-restricted-addons \
+			   vlc \
+			   zim
+
+	# install snap packages
+	sudo snap install multipass
+}
+
+server_install_packages() {
+	sudo apt install -y docker.io
+	sudo usermod -aG docker $USER
+}
+
+install_packages () {
+	# update source list and source code list
+	sudo sed -i s/ca.archive.ubuntu.com/mirror.it.ubc.ca/g /etc/apt/sources.list.d/ubuntu.sources
+	sudo sed -i 's/deb/deb deb-src/g' /etc/apt/sources.list.d/ubuntu.sources
+	sudo apt update && sudo apt -y upgrade
+
+	generic_install_packages
+
+	if dpkg -l | grep -q ubuntu-desktop ; then
+		destkop_install_packages
+
+		# install Google Chrome
+		if ! which google-chrome > /dev/null ; then
+			wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+			sudo dpkg -i google-chrome-stable_current_amd64.deb
+			sudo apt install -f -y
+			rm google-chrome-stable_current_amd64.deb
+		fi
+	else
+		server_install_packages
+	fi
+
+	# remove pre-installed applications
+	sudo apt purge -y chromium-browser rhythmbox transmission-common aisleriot \
+			gnome-mahjongg gnome-mines gnome-sudoku totem
+
+	sudo apt -y autoremove
+}
+
+gnome_config () {
+	# install applications for Gnome DE
+	sudo apt install -y network-manager-openvpn-gnome gnome-shell-extensions \
+			    gnome-shell-extension-manager gnome-tweaks gnome-weather
+
+	# disable natural scrolling
+	gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll false
+
+	# set wheel scroll
+	gsettings set org.gnome.desktop.peripherals.trackball scroll-wheel-emulation-button 8
+
+	# hide desktop icons
+	gsettings set org.gnome.shell.extensions.ding show-home false
+	gsettings set org.gnome.shell.extensions.ding show-trash false
+
+	# hide dock icons
+	gsettings set org.gnome.shell.extensions.dash-to-dock show-trash false
+	gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts false
+}
+
+system_config () {
+	# disable crash report / apport
+	sudo rm /var/crash/*
+	sudo sed -i -e s/^enabled\=1$/enabled\=0/ /etc/default/apport
+
+	# blacklist webcam for security
+	echo "blacklist uvcvideo" | sudo tee -a /etc/modprobe.d/blacklist.conf
+
+	# desktop only below
+	if ! dpkg -l ubuntu-desktop &> /dev/null ; then
+		return
+	fi
+
+	# create "Shared" and "tmp" directories
+	cd $HOME
+	[ -e Shared ] || mkdir Shared
+	[ -e tmp ] || mkdir tmp
+
+	if echo $XDG_CURRENT_DESKTOP | grep -qi gnome ; then
+		gnome_config
+	fi
+
+	# setup for tilix
+	if which tilix > /dev/null ; then
+		sudo ln -s /etc/profile.d/vte-2.91.sh /etc/profile.d/vte.sh
+	fi
+}
+
 # assign default directories if there aren't any
 SOURCE_DIRECTORY=${1:-'src'}
 
-# update source list and source code list
-sudo sed -i s/ca.archive.ubuntu.com/mirror.it.ubc.ca/g /etc/apt/sources.list.d/ubuntu.sources
-sudo sed -i 's/deb/deb deb-src/g' /etc/apt/sources.list.d/ubuntu.sources
+# install packages based on system configs
+install_packages
 
-sudo apt update && sudo apt -y upgrade
+# configuration based on desktop
+system_config
 
-sudo apt install -y acpica-tools vim git git-email gitk openssh-server tree \
-		    meld ibus-chewing unp p7zip-full pastebinit screen preload \
-		    network-manager-openvpn-gnome thunderbird tilix htop zim \
-		    curl numix-gtk-theme numix-icon-theme-circle openconnect \
-		    shellcheck linux-tools-generic linux-tools-`uname -r` \
-		    gnome-shell-extensions gnome-shell-extension-manager \
-		    drm-info mpv iw plocate gnome-weather crudini nvme-cli \
-		    lm-sensors btop python3-github python3-virtualenv remmina \
-		    python3-gpg libfuse-dev fzf fd-find nvtop
-
+# download source code
 cd $HOME
 [ -e $SOURCE_DIRECTORY ] || mkdir $SOURCE_DIRECTORY
 cd $SOURCE_DIRECTORY
 
-# get source code
 [ -e source_scripts ] || git clone https://github.com/alexhungce/source_scripts.git
 [ -e system_scripts ] || git clone https://github.com/alexhungce/system_scripts.git
-
-# create "Shared" directory for VMs
-cd $HOME
-[ -e Shared ] || mkdir Shared
-
-# disable crash report / apport
-sudo rm /var/crash/*
-sudo sed -i -e s/^enabled\=1$/enabled\=0/ /etc/default/apport
-
-# blacklist webcam for security
-echo "blacklist uvcvideo" | sudo tee -a /etc/modprobe.d/blacklist.conf
-
-# install different packages if not in VirtualBox
-sudo dmidecode -t system | grep -n VirtualBox
-if [ $? != 0 ] ; then
-	sudo apt install -y vlc acpi deluge powertop ubuntu-restricted-addons avahi-daemon
-fi
-
-# remove pre-installed applications
-sudo apt purge -y chromium-browser rhythmbox transmission-common aisleriot \
-		  gnome-mahjongg gnome-mines gnome-sudoku totem
-
-# install Google Chrome
-if ! which google-chrome > /dev/null ; then
-	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-	sudo dpkg -i google-chrome-stable_current_amd64.deb
-	sudo apt install -f -y
-	rm google-chrome-stable_current_amd64.deb
-fi
-
-# install applications for Gnome DE
-sudo apt install -y gnome-tweaks
-
-# setup for tilix
-if which tilix > /dev/null ; then
-	sudo ln -s /etc/profile.d/vte-2.91.sh /etc/profile.d/vte.sh
-fi
-
-sudo apt -y autoremove
-
-# disable natural scrolling
-gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll false
-
-# set wheel scroll
-gsettings set org.gnome.desktop.peripherals.trackball scroll-wheel-emulation-button 8
-
-# hide desktop icons
-gsettings set org.gnome.shell.extensions.ding show-home false
-gsettings set org.gnome.shell.extensions.ding show-trash false
-
-# hide dock icons
-gsettings set org.gnome.shell.extensions.dash-to-dock show-trash false
-gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts false
-
-# for faster boot time
-sudo systemctl mask NetworkManager-wait-online.service
