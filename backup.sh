532ede67a996995dddd76fbe97d9eea26260b4cb
Alex Hung <alexhung@gmail.com>
2025-12-29 21:02:16 -0700

    backup: Replace comments and echos with log outputs
    Signed-off-by: Alex Hung <alexhung@gmail.com>

--- backup.sh
+++ backup.sh
@@ -14,13 +14,21 @@ BACKUP_FILE_LIST=( msmtprc pwclientrc gnupg ssh vim sesame vimrc lnxpromote \
 		   bash_aliases bash_dev bash_igtops bash_kernelops bash_misc bash_servers )
 BACKUP_CONFIG_LIST=( ghostty mpv tilix zim )
 
-echo "creating a backup directory and subdirectories..."
+# Helper functions
+log() {
+    echo -e "\033[1;32m[BACKUP] $1\033[0m"
+}
+
+warn() {
+    echo -e "\033[1;33m[WARNING] $1\033[0m"
+}
+
+log "initializing backup directories..."
 cd $HOME
 mkdir -p "$BACKUP_DIR/$DOT_FILE"/{.config,gitconfig,vscode} "$BACKUP_DIR/$CONFIG_FILE"
 
 # compress .atom, .mozilla and .thunderbird
-echo "creating archives..."
-
+log "creating application archives..."
 for dir in "${BACKUP_DIR_LIST[@]}"
 do
 	tar -zcf $BACKUP_DIR/${dir}.tar.gz .${dir}
@@ -31,8 +39,7 @@ do
 	tar -zcf $BACKUP_DIR/${dir}.tar.gz snap/${dir}
 done
 
-# copy hidden files
-echo "copying dot files and directories..."
+log "copying dot files and directories..."
 for file in "${BACKUP_FILE_LIST[@]}"
 do
 	[ -f .${file} ] && cp -fp .${file} $BACKUP_DIR/$DOT_FILE
@@ -44,23 +51,23 @@ do
 	[ -d .config/${file} ] && cp -frp .config/${file} $BACKUP_DIR/$DOT_FILE/.config/
 done
 
-echo "copying .gitconfig in each directories..."
+log "copying .gitconfig in all directories..."
 [ -e .gitconfig ] && cp -fp .gitconfig $BACKUP_DIR/$DOT_FILE/gitconfig/.gitconfig
 [ -e src/amd/.gitconfig ] && cp -fp src/amd/.gitconfig $BACKUP_DIR/$DOT_FILE/gitconfig/.gitconfig_amd
 [ -e src/personal/.gitconfig ] && cp -fp src/personal/.gitconfig $BACKUP_DIR/$DOT_FILE/gitconfig/.gitconfig_personal
 
-echo "copying VSCode config..."
+log "copying VSCode config..."
 [ -d "$VSCODE_CONFIG" ] && cp -fp "$VSCODE_CONFIG/"*.json "$VSCODE_BACKUP/"
 [ -d "$VSCODE_CONFIG/snippets" ] && cp -r "$VSCODE_CONFIG/snippets" "$VSCODE_BACKUP/"
 
-echo "copying config files..."
-# backup tilix config
+log "copying application config files..."
 dconf dump /com/gexperts/Tilix/ > $BACKUP_DIR/$CONFIG_FILE/tilix.dconf
 
-echo "compressing hidden and config directories"
+log "finalizing backup archives..."
 cd $BACKUP_DIR
 tar -zcf $DOT_FILE.tar.gz $DOT_FILE
 tar -zcf $CONFIG_FILE.tar.gz $CONFIG_FILE
 rm -rf $DOT_FILE
 rm -rf $CONFIG_FILE
 
+log "Backup completed successfully at $BACKUP_DIR"
